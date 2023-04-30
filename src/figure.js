2ad407da1cb32dcc399458e2ada89db047b97cbf
Rafael Armenta <rarmenta@hawk.iit.edu>
2023-04-30 04:10:54 -0500

    created figure class

---
+++ src/figure.js
@@ -0,0 +1,16 @@
+class Figure {
+  constructor(id, seriesName, figureName, version, scale, manufacturer, releaseDate, urlPage)  {
+    this.id = id;
+    this.seriesName = seriesName;
+    this.figureName = figureName;
+    this.version = version;
+    this.scale = scale;
+    this.manufacturer = manufacturer;
+    this.releaseDate = releaseDate;
+    this.urlPage = urlPage;
+  }
+
+
+}
+
+module.exports = Figure;
